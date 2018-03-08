0829531123a2682fe05dff6dd1ab581f4eb1b65a
micxye <michaelyeahh@gmail.com>
2018-03-08 14:17:15 -0800

    Complete linkedlist

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -7,13 +7,13 @@ var LinkedList = function() {
   list.addToTail = function(value) {
     //takes a value and adds it to the end of the list
     var newNode = Node(value);
-    if (list.head === null) {
+    if (list.head === null) { //first entry
       list.head = newNode;
       list.tail = newNode;
-    } else if (list.head === list.tail) {
+    } else if (list.head === list.tail) { //second entry
       list.tail = newNode;
-      list.head.next == newNode 
-    } else {
+      list.head.next = newNode;
+    } else { // all other entries
       list.tail.next = newNode;
       list.tail = newNode;
     }
@@ -22,13 +22,34 @@ var LinkedList = function() {
 
   list.removeHead = function() {
     //removes the first node from the list and returns its value
-    var removedVal = list.head.value
+    var removedVal = list.head.value;
     list.head = list.head.next;
     return removedVal;
   };
 
   list.contains = function(target) {
     // returns boolean reflecting whether or not the passed-in value is in the list
+    var currentObj;
+    
+    var traverse = function (currentObj) {
+      if (currentObj === list.tail && currentObj.value !== target) {
+        return false;
+      }
+      
+      if (currentObj === undefined) {
+        currentObj = list.head;
+      } else {
+        currentObj = currentObj.next;
+      }
+      
+      if (currentObj.value === target) {
+        return true;
+      } else {
+        return traverse(currentObj);
+      }
+    };
+    
+    return traverse(currentObj);
   };
 
   return list;
