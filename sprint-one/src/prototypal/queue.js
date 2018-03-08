4c352b197ee9e93a499c7fe15c625d1e103c2abf
Michael Ye <micxye@yahoo.com>
2018-03-07 16:10:21 -0800

    finished implementing prototypal version of stack data structure

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,38 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance.storage = {};
+  someInstance.queueSize = 0;
+  someInstance.firstKey = '0';
+  someInstance.currentKey = '0';
+  someInstance.lastDequeued;
+  return someInstance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  if (this.queueSize === 0) {
+    this.storage[this.firstKey] = value;
+    this.queueSize++;
+  } else {
+    this.currentKey = (Number(this.currentKey) + 1).toString();
+    this.storage[this.currentKey] = value;
+    this.queueSize++;
+  }
+};
 
+queueMethods.dequeue = function() {
+  if (this.queueSize > 0) {
+    this.queueSize--;
+  }
+  this.lastDequeued = this.storage[this.firstKey];
+  delete this.storage[this.firstKey];
+  if (this.currentKey !== this.firstKey) {
+    this.firstKey = (Number(this.firstKey) + 1).toString();
+  }
+  return this.lastDequeued;
+};
+
+queueMethods.size = function() {
+  return this.queueSize;
+};
