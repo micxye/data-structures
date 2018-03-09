e0b4b6daeb78ad9f3df050a495d4a8b53a89f7f7
micxye <michaelyeahh@gmail.com>
2018-03-09 11:25:22 -0800

    complete bare minimum for hash table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,38 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (!Array.isArray(this._storage[index])) {
+    this._storage[index] = [];
+  }
+  var bucket = this._storage[index];
+  if (bucket) {
+    if (this.retrieve(k)) {
+      for (var i = 0; i < bucket.length; i++) {
+        if (bucket[i][0] === k) {
+          bucket[i][1] = v;
+        }
+      }
+    }
+  }
+  var toPush = [k, v];
+  this._storage[index].push(toPush);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage[index];
+  if (bucket) {
+    for (var i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === k) {
+        return bucket[i][1];
+      }
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage[index] = undefined;
 };
 
 
